1d225636d47543229ef0278b1952e8ef682b0d53
MadMoth125 <madmoth125@gmail.com>
2024-01-21 15:41:40 -0600

    demo files

---
+++ JSON-Testing/JSON-Testing/Classes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Testing.Classes
+{
+	/// <summary>
+	/// An example class to demonstrate JSON serialization.
+	/// </summary>
+	public class Person
+	{
+		// constructor
+		public Person(string name, int age, string city)
+		{
+			Name = name;
+			Age = age;
+			City = city;
+		}
+
+		// properties (getters)
+		public string Name { get; } = "";
+		public int Age { get; } = 0;
+		public string City { get; } = "";
+	}
+}
---
+++ JSON-Testing/JSON-Testing/Classes/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Testing.Classes
+{
+	/// <summary>
+	/// An example class to demonstrate JSON serialization.
+	/// </summary>
+	public class Product
+	{
+		// constructor
+		public Product(string name, int price, CategoryType category)
+		{
+			Name = name;
+			Price = price;
+			Category = category;
+		}
+
+		// properties (getters)
+		public string Name { get; } = "";
+		public int Price { get; } = 0;
+		public CategoryType Category { get; } = CategoryType.Other;
+
+		// enum for demo purposes
+		public enum CategoryType
+		{
+			Food,
+			Beverage,
+			Other
+		}
+	}
+}
